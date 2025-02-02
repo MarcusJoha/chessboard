443468b22f3f7cf0c211a91582c60350c9c83bf8
Marcus Johannessen <marcus-j@online.no>
2025-02-02 16:53:55 +0100

    changed port

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port:5080,
+    port:4173,
     host:true,
     strictPort:true,
   },
